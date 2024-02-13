9a075063b0564f28b24ba06f5810ef7b09f20a3a
Matt Cowles <selwocm@gmail.com>
2024-02-13 10:33:25 -0800

    Installed C1.Win.Barcode and added barcode to form

--- WinFormsApp32/Form1.Designer.cs
+++ WinFormsApp32/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             c1FlexGrid1 = new C1.Win.FlexGrid.C1FlexGrid();
             c1FlexGrid2 = new C1.Win.FlexGrid.C1FlexGrid();
+            c1BarCode1 = new C1.Win.BarCode.C1BarCode();
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid2).BeginInit();
             SuspendLayout();
@@ -51,11 +52,31 @@
             c1FlexGrid2.Size = new Size(360, 225);
             c1FlexGrid2.TabIndex = 1;
             // 
+            // c1BarCode1
+            // 
+            c1BarCode1.AdditionalNumber = null;
+            c1BarCode1.BackColor = Color.White;
+            c1BarCode1.Font = new Font("Microsoft Sans Serif", 12F);
+            c1BarCode1.ForeColor = Color.Black;
+            c1BarCode1.Location = new Point(161, 549);
+            c1BarCode1.MicroQRCodeOptions.EncodingCodePage = 65001;
+            c1BarCode1.Name = "c1BarCode1";
+            c1BarCode1.QuietZone.Bottom = 0D;
+            c1BarCode1.QuietZone.Left = 0D;
+            c1BarCode1.QuietZone.Right = 0D;
+            c1BarCode1.QuietZone.Top = 0D;
+            c1BarCode1.Size = new Size(247, 40);
+            c1BarCode1.TabIndex = 2;
+            c1BarCode1.Text = "c1BarCode1";
+            c1BarCode1.TextFixedLength = 0;
+            c1BarCode1.WideToNarrowRatio = 2F;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1143, 750);
+            Controls.Add(c1BarCode1);
             Controls.Add(c1FlexGrid2);
             Controls.Add(c1FlexGrid1);
             Margin = new Padding(4, 5, 4, 5);
@@ -64,11 +85,13 @@
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).EndInit();
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid1;
         private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid2;
+        private C1.Win.BarCode.C1BarCode c1BarCode1;
     }
 }
