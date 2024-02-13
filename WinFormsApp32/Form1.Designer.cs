a2c3b334feb49c19e89d25021d71d0243ef81714
hunter.haaf <hunter.haaf@HUNTERHAAF>
2024-02-13 13:00:49 -0500

    Add project files.

---
+++ WinFormsApp32/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp32
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            c1FlexGrid1 = new C1.Win.FlexGrid.C1FlexGrid();
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).BeginInit();
+            SuspendLayout();
+            // 
+            // c1FlexGrid1
+            // 
+            c1FlexGrid1.ColumnInfo = "10,1,0,0,0,-1,Columns:";
+            c1FlexGrid1.Location = new Point(28, 25);
+            c1FlexGrid1.Name = "c1FlexGrid1";
+            c1FlexGrid1.Size = new Size(240, 150);
+            c1FlexGrid1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(c1FlexGrid1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid1;
+    }
+}
