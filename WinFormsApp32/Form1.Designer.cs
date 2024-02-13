559317fd33932659bbb87e527764091be19f1389
Matt Cowles <selwocm@gmail.com>
2024-02-13 10:16:14 -0800

    Added second flexchart to form

--- WinFormsApp32/Form1.Designer.cs
+++ WinFormsApp32/Form1.Designer.cs
@@ -29,31 +29,46 @@
         private void InitializeComponent()
         {
             c1FlexGrid1 = new C1.Win.FlexGrid.C1FlexGrid();
+            c1FlexGrid2 = new C1.Win.FlexGrid.C1FlexGrid();
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid2).BeginInit();
             SuspendLayout();
             // 
             // c1FlexGrid1
             // 
             c1FlexGrid1.ColumnInfo = "10,1,0,0,0,-1,Columns:";
-            c1FlexGrid1.Location = new Point(28, 25);
+            c1FlexGrid1.Location = new Point(40, 42);
+            c1FlexGrid1.Margin = new Padding(4, 5, 4, 5);
             c1FlexGrid1.Name = "c1FlexGrid1";
-            c1FlexGrid1.Size = new Size(240, 150);
+            c1FlexGrid1.Size = new Size(342, 249);
             c1FlexGrid1.TabIndex = 0;
+            // 
+            // c1FlexGrid2
+            // 
+            c1FlexGrid2.ColumnInfo = "10,1,0,0,0,-1,Columns:";
+            c1FlexGrid2.Location = new Point(612, 456);
+            c1FlexGrid2.Name = "c1FlexGrid2";
+            c1FlexGrid2.Size = new Size(360, 225);
+            c1FlexGrid2.TabIndex = 1;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1143, 750);
+            Controls.Add(c1FlexGrid2);
             Controls.Add(c1FlexGrid1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)c1FlexGrid1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)c1FlexGrid2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid1;
+        private C1.Win.FlexGrid.C1FlexGrid c1FlexGrid2;
     }
 }
